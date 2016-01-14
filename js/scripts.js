c972fbcfd28183b5bceaffab3709b9ee5048e807
matt knutson and wren withers <mknut001@gmail.com>
2016-01-14 14:27:51 -0800

    create wiki page

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+  $('#markdown').click(function() {
+    window.location='pages/markdown.html'
+  });
+  $('#block').click(function() {
+    window.location='pages/block.html'
+  });
+  $('#inline').click(function() {
+    window.location='pages/inline.html'
+  });
+  $('#css').click(function() {
+    window.location='pages/css.html'
+  });
+  $('#bootstrap').click(function() {
+    window.location='pages/bootstrap.html'
+  });
+  $('#divs').click(function() {
+    window.location='pages/divs.html'
+  });
+  $('#floats').click(function() {
+    window.location='pages/floats.html'
+  });
+  $('#box').click(function() {
+    window.location='pages/box.html'
+  });
+  $('#cascading').click(function() {
+    window.location='pages/cascading.html'
+  });
+  $('#grid').click(function() {
+    window.location='pages/grid.html'
+  });
+  $('#responsive').click(function() {
+    window.location='pages/responsive.html'
+  });
+  $('#javascript').click(function() {
+    window.location='pages/javascript.html'
+  });
+  $('#jsvar').click(function() {
+    window.location='pages/jsvar.html'
+  });
+  $('#jsmeth').click(function() {
+    window.location='pages/jsmeth.html'
+  });
+  $('#jsstring').click(function() {
+    window.location='pages/jsstring.html'
+  });
+  $('#jsfunc').click(function() {
+    window.location='pages/jsfunc.html'
+  });
+  $('#jquery').click(function() {
+    window.location='pages/jquery.html'
+  });
+  $('#dom').click(function() {
+    window.location='pages/dom.html'
+  });
+  $('#forms').click(function() {
+    window.location='pages/forms.html'
+  });
+  $('#attributes').click(function() {
+    window.location='pages/attributes.html'
+  });
+  $('#branching').click(function() {
+    window.location='pages/branching.html'
+  });
+  $('#arrays').click(function() {
+    window.location='pages/arrays.html'
+  });
+  $('#looping').click(function() {
+    window.location='pages/looping.html'
+  });
+});
